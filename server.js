840006ced4228bf887fa742e468f131e2aa7ffbe
A1webtechies <qalibabbasf18@gmail.com>
2018-09-25 06:35:02 +0500

    setting up projects page

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ app.get('/about' , (req,res)=>{
         pageTitle: 'About Page',
     })
 })
+app.get('/projects' , (req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle: 'Projecst'
+    })
+})
 app.get('/bad' , (req,res)=>{
     res.send({
         errorMessage: 'Unable to load Page',
